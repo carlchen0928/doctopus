45e6d1ca77e82a5df4550f9d02f23db315f9f2a7
udms <udms@udms-155.(none)>
2014-09-03 20:12:16 +0800

    add files

--- apps/urlcrawler/models.py
+++ apps/urlcrawler/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+import pymongo
+import mongoengine as mongo
+from bson import ObjectId
 
 
 class urlTask(models.Model):
     task_id = models.IntegerField(primary_key=True)
-    task_name = models.CharField()
+    task_name = models.CharField(max_length=100)
     task_filepath = models.CharField(max_length=100)
     max_depth = models.IntegerField()
     url_filter = models.CharField(max_length=200, blank=True)
@@ -12,4 +15,16 @@ class urlTask(models.Model):
         db_table = 'taskinfo'
 
 
+class DDoc(mongo.Document):
+    task_id = mongo.IntField()
+    from_url = mongo.StringField(max_length=1024)
+    page_url = mongo.StringField(max_length=1024)
+    page_content = mongo.StringField()
+    page_level = mongo.IntField()
+    download_date = mongo.DateTimeField()
+
+    meta = {
+        'collection': 'doctopus', 
+        'ordering': ['-download_date']
+    }
 # Create your models here.
