93dabc9752b1d7e6c31227a4260340b143832592
udms <udms@udms-131.(none)>
2014-09-05 05:58:34 +0800

    finish main function of tasks

---
+++ apps/urlcrawler/tasks.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from celery import Celery
+from celery.utils.log import get_task_logger
+import pyreBloom
+import tldextract
+import time
+
+from utils import Fetch_and_parse_and_store
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+app = Celery('tasks', broker='redis://172.21.1.155')
+logger = get_task_logger(__name__)
+
+@app.task
+def retrieve_page(task_id, url, depth, now_depth, allow_domains=None):
+
+	# Filter the url that has been crawled
+	p = pyreBloom.pyreBloom('task%d' % task_id, 100000, 0.01, host='172.21.1.155')
+	if p.contains(url):
+		return
+
+	# start crawling...
+	fps = Fetch_and_parse_and_store(task_id, url, depth, now_depth, allow_domains, __name__)
+#	p.extend(url)
+	
+	if fps.fetch() == True:
+		fps.store()
+		fps.follow_links()
+
+	
